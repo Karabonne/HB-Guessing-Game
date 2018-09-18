54a299046fd7d45dd2dc14b40727150faf4bd2af
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-18 16:20:11 -0700

    Ask if want to play again and store best score

--- game.py
+++ game.py
@@ -5,29 +5,43 @@ print("welcome!")
 name = input("what is your name? ")
 print(f"Hi {name}! Welcome to this really cool guessing game! Pick a number between 1 and 100: ")
 
-answer = random.randint(1, 101)
-number_of_guesses = 0
+#answer = random.randint(1, 101)
+answer = 50
 correct = False
-excepted = True
+top_score = 0
+number_of_guesses = 0
+first_game = True
 while correct == False:
-    while True: 
-        try:
-            guess = int(input())
-            if 1 <= guess <= 100:
-                number_of_guesses += 1
-                if guess < answer: 
-                    print("Too low! Try again.")
-                elif guess > answer: 
-                    print("Too high! Try again.")
-                else: 
-                    print("Correct! Nice job.")
-                    print("It took you {} guesses!".format(number_of_guesses))
-                    correct = True
-                    break
+    try:
+        guess = int(input("guess: "))
+    except ValueError:
+        print("integers only, c'mon!")
+        continue
+    if 1 <= guess <= 100:
+        number_of_guesses += 1
+        if guess < answer: 
+            print("Too low! Try again.")
+        elif guess > answer: 
+            print("Too high! Try again.")
+        else: 
+            print("Correct! Nice job.")
+            print("It took you {} guesses!".format(number_of_guesses))
+            while first_game == True:
+                top_score = number_of_guesses
+                first_game = False
+            if number_of_guesses <= top_score:
+                top_score = number_of_guesses
+            print(f"Your all time top score is {top_score}.")
+            new_game = input("New game? Enter yes, anything else will exit: ")
+            if new_game == "yes":
+                #answer = random.randint(1, 101)
+                number_of_guesses = 0
+                continue
             else:
-                print("Please enter a valid guess this time, yo. :)")
-        except ValueError:
-            print("integers only, c'mon!")
+                correct = True
+    else:
+        print("Please enter a valid guess this time, yo. :)")
+    
 
 
 
