b062021b325f4c623ea69ddc741148f50dbdf2ec
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-18 13:02:30 -0700

    Added input check

--- game.py
+++ game.py
@@ -7,19 +7,22 @@ print("Hi {}! Welcome to this really cool guessing game! Pick a number between 1
 
 answer = random.randint(1, 101)
 number_of_guesses = 0
-while True: 
+correct = False
+while correct == False:
+    
     guess = int(input())
-    if guess < answer: 
-        print("Too low! Try again.")
-        number_of_guesses += 1
-    elif guess > answer: 
-        print("Too high! Try again.")
-        number_of_guesses += 1
-    else: 
-        print("Correct! Nice job.")
-        number_of_guesses += 1
-        print("It took you {} guesses!".format(number_of_guesses))
-        break
+    number_of_guesses += 1
+    if 1 <= guess <= 100:
+        if guess < answer: 
+            print("Too low! Try again.")
+        elif guess > answer: 
+            print("Too high! Try again.")
+        else: 
+            print("Correct! Nice job.")
+            print("It took you {} guesses!".format(number_of_guesses))
+            correct = True
+    else:
+        print("Please enter a valid guess this time, yo. :)")
 
 
 #repeat forever:
