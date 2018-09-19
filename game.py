cabe8864c4d2fc2e8446788924ed4e421648423f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-18 17:56:41 -0700

    added mode select framework

--- game.py
+++ game.py
@@ -1,46 +1,110 @@
 """A number-guessing game."""
 import random
 
-print("welcome!")
-name = input("what is your name? ")
-print(f"Hi {name}! Welcome to this really cool guessing game! Pick a number between 1 and 100: ")
 
-#answer = random.randint(1, 101)
-answer = 50
+lower_bound = int(input("Set the lower bound: "))
+upper_bound = int(input("Set the upper bound: "))
+while upper_bound <= lower_bound:
+    print("Please make sure the upper bound is actually greater than the lower one...")
+    upper_bound = int(input("Set the upper bound: "))
+
+
 correct = False
 top_score = 0
 number_of_guesses = 0
 first_game = True
-while correct == False:
-    try:
-        guess = int(input("guess: "))
-    except ValueError:
-        print("integers only, c'mon!")
-        continue
-    if 1 <= guess <= 100:
-        number_of_guesses += 1
-        if guess < answer: 
-            print("Too low! Try again.")
-        elif guess > answer: 
-            print("Too high! Try again.")
-        else: 
-            print("Correct! Nice job.")
-            print("It took you {} guesses!".format(number_of_guesses))
-            while first_game == True:
-                top_score = number_of_guesses
-                first_game = False
-            if number_of_guesses <= top_score:
-                top_score = number_of_guesses
-            print(f"Your all time top score is {top_score}.")
-            new_game = input("New game? Enter yes, anything else will exit: ")
-            if new_game == "yes":
-                #answer = random.randint(1, 101)
-                number_of_guesses = 0
-                continue
+
+print("welcome!")
+name = input("what is your name? ")
+lower_bound = int(input("Set the lower bound: "))
+upper_bound = int(input("Set the upper bound: "))
+while upper_bound <= lower_bound:
+    print("Please make sure the upper bound is actually greater than the lower one...")
+    upper_bound = int(input("Set the upper bound: "))
+
+print(f"Hi {name}! Welcome to this really cool guessing game! Pick your game mode.")
+mode = input("Enter 1 for Guessing CPU Number, enter 2 for CPU Guessing Your Number: ")
+if mode == 1:
+    game_mode_1()
+elif mode == 2:
+    game_mode_2()
+else:
+
+
+def game_mode_1():
+    answer = random.randint(lower_bound, upper_bound)
+    while correct == False:
+        try:
+            guess = int(input("guess: "))
+        except ValueError:
+            print("integers only, c'mon!")
+            continue
+        if lower_bound <= guess <= upper_bound:
+            number_of_guesses += 1
+            if number_of_guesses >= 10:
+                print("Too many guesses, sorry :(. Exiting...")
+                break
             else:
-                correct = True
-    else:
-        print("Please enter a valid guess this time, yo. :)")
+                if guess < answer: 
+                    print("Too low! Try again.")
+                elif guess > answer: 
+                    print("Too high! Try again.")
+                else: 
+                    print("Correct! Nice job.")
+                    print(f"It took you {number_of_guesses} guesses!")
+                    while first_game == True:
+                        top_score = number_of_guesses
+                        first_game = False
+                    if number_of_guesses <= top_score:
+                        top_score = number_of_guesses
+                    print(f"Your all time top score is {top_score}, in a range of {lower_bound} to {upper_bound}.")
+                    new_game = input("New game? Enter yes, anything else will exit: ")
+                    if new_game == "yes":
+                        answer = random.randint(lower_bound, upper_bound)
+                        number_of_guesses = 0
+                        continue
+                    else:
+                        correct = True
+        else:
+            print("Please enter a valid guess this time, yo. :)")
+
+def game_mode_2():
+    print("In this version, the computer will guess your number. You will tell it the upper bound and lower bound.")
+    lower_bound = int(input("your lower bound: "))
+    upper_bound = int(input("your upper bound: "))
+    
+    guess = random.randint(lower_bound, upper_bound)
+    print(f"Computer's guess is {guess}")
+            if lower_bound <= guess <= upper_bound:
+            number_of_guesses += 1
+            if number_of_guesses >= 10:
+                print("Too many guesses, sorry :(. Exiting...")
+                break
+            else:
+                if guess < answer: 
+                    print("Too low! Try again.")
+                elif guess > answer: 
+                    print("Too high! Try again.")
+                else: 
+                    print("Correct! Nice job.")
+                    print(f"It took you {number_of_guesses} guesses!")
+                    while first_game == True:
+                        top_score = number_of_guesses
+                        first_game = False
+                    if number_of_guesses <= top_score:
+                        top_score = number_of_guesses
+                    print(f"Your all time top score is {top_score}, in a range of {lower_bound} to {upper_bound}.")
+                    new_game = input("New game? Enter yes, anything else will exit: ")
+                    if new_game == "yes":
+                        answer = random.randint(lower_bound, upper_bound)
+                        number_of_guesses = 0
+                        continue
+                    else:
+                        correct = True
+
+
+
+
     
 
 
